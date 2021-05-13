555ac6a7e41f883b2d9823e463a4e4d9cc5fa7b0
Sam Urmy <oceanographerschoice@gmail.com>
2021-05-13 16:42:39 -0700

    make global data vectors `const` in example

--- test/example.jl
+++ test/example.jl
@@ -12,15 +12,15 @@ categories = 1:ncategories
 aa = rand(Normal(μ0, σ0), ncategories)
 b = 4.5
 σ = 0.5
-category = repeat(categories, inner=50)
-n = length(category)
-x = rand(Uniform(-1, 1), n)
+const category = repeat(categories, inner=50)
+const n = length(category)
+const x = rand(Uniform(-1, 1), n)
 μ = [aa[category[i]] + b * x[i] for i in 1:n]
-y = rand.(Normal.(μ, σ))
+const y = rand.(Normal.(μ, σ))
 
 scatter(x, y, color=category, label="")
 
-function loglik(θ)
+function loglik(θ::Vector{T}) where T
     μ0 = θ[1]
     σ0 = exp(θ[2])
     aa = θ[3:10]
@@ -38,18 +38,18 @@ loglik(θtrue)
 mll = MarginalLogDensity(loglik, nθ, collect(3:10))
     # Cubature(-10ones(nθ-3), 10ones(nθ-3)))
 mll(aa, [μ0, log(σ0), b, log(σ)])
-mll([μ0, log(σ0), b, log(σ)])
+@profiler for i in 1:100 mll([μ0, log(σ0), b, log(σ)]) end
 mll(randn(4))
 
 opt = optimize(θjoint -> -mll(θjoint), ones(4))
 μ0_opt, logσ0_opt, b_opt, logσ_opt = opt.minimizer
 
-μ0_opt
-exp(logσ0_opt)
-b_opt
-exp(logσ_opt)
+                    # should be:
+μ0_opt              # 5.0
+exp(logσ0_opt)      # 5.0
+b_opt               # 4.5
+exp(logσ_opt)       # 0.5
 
-μ0_opt - μ0
-exp(logσ0_opt) - σ0
-b_opt - b
-exp(logσ_opt) - σ
+using FiniteDiff, LinearAlgebra
+H = FiniteDiff.finite_difference_hessian(θjoint -> -mll(θjoint), opt.minimizer)
+std_errors = 1 ./ sqrt.(diag(H))

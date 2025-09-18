078e6ede9c65624f8fd0179d4644743b205abad0
Sam Urmy <oceanographerschoice@gmail.com>
2025-09-18 20:50:04 +1000

    Start on real documentation pages

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using MarginalLogDensities
+using Documenter
+
+DocMeta.setdocmeta!(MarginalLogDensities, :DocTestSetup, :(using MarginalLogDensities); recursive=true)
+
+makedocs(;
+    modules=[MarginalLogDensities],
+    authors="Sam Urmy <oceanographerschoice@gmail.com> and contributors",
+    repo="https://github.com/ElOceanografo/MarginalLogDensities.jl/blob/{commit}{path}#{line}",
+    sitename="MarginalLogDensities.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://ElOceanografo.github.io/MarginalLogDensities.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/ElOceanografo/MarginalLogDensities.jl",
+    devbranch="master",
+)

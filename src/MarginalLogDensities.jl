c904935691e1c05228eb1a90393665a5b203de86
Sam Urmy <oceanographerschoice@gmail.com>
2025-10-07 20:42:27 +1100

    Change default to TracerSparsityDetector()

--- src/MarginalLogDensities.jl
+++ src/MarginalLogDensities.jl
@@ -195,7 +195,7 @@ _generic_eachindex(x) = eachindex(x)
 _generic_eachindex(x::ComponentArray) = keys(x)
 
 function MarginalLogDensity(logdensity, u, iw, data=(), method=LaplaceApprox(); 
-        hess_adtype=nothing, sparsity_detector=TracerLocalSparsityDetector(),#DenseSparsityDetector(method.adtype, atol=sqrt(eps())),
+        hess_adtype=nothing, sparsity_detector=TracerSparsityDetector(),
         coloring_algorithm=GreedyColoringAlgorithm())
     iv = setdiff(_generic_eachindex(u), iw)
     w = u[iw]
